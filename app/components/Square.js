e9bd4cd24118b39d3ff38e02aa96686e67e5d34e
natac13 <sean.campbell13@gmail.com>
2015-12-11 20:34:51 -0500

    styled the squares and game board a bit

--- app/components/Square.js
+++ app/components/Square.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import classnames from 'classnames';
 
 const Square = ({ placeMarker, square, marker }) => {
+    let squareClass = classnames({
+        'square': true,
+        'active': !!marker,
+        'user': marker === 'X',
+        'comp': marker === 'O'
+    });
 
     return (
-        <button className="square" onClick={placeMarker} id={square}>
+
+        <button className={squareClass} onClick={placeMarker} id={square}>
             {!!marker && marker}
         </button>
     );
